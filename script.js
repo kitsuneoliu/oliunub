0077ebf3b9d4cbaaef9da527fea980d68127394f
kitsuneoliu <nghiahuunguyen2008@gmail.com>
2026-07-22 16:56:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ const roles = [
     { name: "Wolf Summoner", img: "Wolf_Summoner.webp", desc: "Sói triệu hồi" },
     { name: "Wolf Time Traveler", img: "Wolf_Time_Traveler_Icon.svg", desc: "Sói thời gian" },
     { name: "Wolf Trickster", img: "Wolf_trickster.webp", desc: "Sói lừa đảo" }
+    { name: "Firework Wolf", img: "Firework_Wolf.webp", desc: "Sói pháo hoa" }
     // anh giữ nguyên toàn bộ danh sách của anh
 ];
 
